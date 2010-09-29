54877f71fcdce218be91bf05f0e263800766fc3e
Cláudio de Miranda Luz <claudiomluz@gmail.com>
2010-09-29 20:34:36 -0300

    produto.js mudaimagens

---
+++ script/produto.js
@@ -0,0 +1,7 @@
+
+
+$(document).ready(function(){
+    mudaImagem("preto", "images/produto_foto_grande_1.jpg");
+    mudaImagem("azul", "images/produto_foto_grande_2.jpg");
+    mudaImagem("cinza", "images/produto_foto_grande_3.jpg");
+});
